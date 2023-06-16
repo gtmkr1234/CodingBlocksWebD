64e5b63caab57e4859af4071b0ee5596ef9e3ce2
gtmkr1234 <gtmkr1234@gmail.com>
2023-06-16 20:08:43 +0530

    init

---
+++ a.js
@@ -0,0 +1,17 @@
+let a = 55;
+console.log(a);
+
+// Primitive Dayatype in JS
+// Number
+// String
+// Boolean
+// undefined
+// null
+// BigInt
+// Symbol
+
+let b = '1234';
+console.log(typeof b);
+
+var v = 12344;
+console.log(typeof v);
