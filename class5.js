3b53f034d377d0b29961c8ed9d37679e57047476
gtmkr1234 <gtmkr1234@gmail.com>
2023-06-26 21:54:02 +0530

    added

--- class5.js
+++ class5.js
@@ -1,8 +1,16 @@
-function a(fn){
+function a(){
     console.log('a');
-    fn()
+    function b(){
+        console.log('b');
+    }
+    return b;
 }
-function b(){
-    console.log('b');
-}
-a(b);
+
+// function b(){
+//     console.log('b');
+// }
+// a(b);  Higher Order Functions
+
+
+// a()(); // Calling the function b that is returned by function a 
+
