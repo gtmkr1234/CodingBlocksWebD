1f39240cab63914c3682e5bd6b63aec1ee5c48ac
gtmkr1234 <gtmkr1234@gmail.com>
2023-07-05 10:38:47 +0530

    hi

--- public/index.js
+++ public/index.js
@@ -4,9 +4,10 @@ const a = document.querySelectorAll('h1');
 a.forEach(s => {
     s.style.color = 'red';
 });
+const b = document.querySelector('h1');
 
 console.log(a);
-
+console.log(b.innerText);
 
 // document.querySelector returns a single element
 // but document.querySelectorAll returns an array
